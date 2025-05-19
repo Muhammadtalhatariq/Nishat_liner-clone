0addd0d1edaff61a9a94c3563b42d23a4064911f
Muhammad Talha Tariq <138753838+Muhammadtalhatariq@users.noreply.github.com>
2025-05-19 10:11:08 +0500

    completed

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 const TopHeader: React.FC = () => {
   return (
     <div>
-      <p className="text-center bg-[#F6F6F8] p-2 text-[12px] cursor-pointer font-mont">
+      <p className="text-center bg-[#F6F6F8] p-2 px-4 mt-4 text-[12px] cursor-pointer font-mont">
         Designed for the Bold, Created for the Fearless - Wear Confidence, Own
         the Spotlight.
         <span className="text-[#FF0000]"> Shop Now!</span>
